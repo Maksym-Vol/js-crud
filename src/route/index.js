ce8dbcf4187f6b3232d5a03366675181708682c5
p1 <you@example.com>
2023-09-14 17:28:17 +0300

    done

--- src/route/index.js
+++ src/route/index.js
@@ -11,12 +11,43 @@ class User {
     this.email = email
     this.login = login
     this.password = password
+    this.id = new Date().getTime()
   }
+
+  verifyPassword = (password) => this.password === password
+
   static add = (user) => {
     this.#list.push(user)
   }
-  static getList = () => {
-    return this.#list
+  static getList = () => this.#list
+  
+  static getById = (id) => this.#list.find((user) => user.id === id)
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (user) => user.id ===id,
+    )
+
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  } 
+  static updateById = (id, email) => {
+    const user = this.getById(id);
+    if (user) {
+      this.update(user, data)
+      return true
+    } else {
+      return false
+    }
+  }
+  static update = (user, { email }) => {
+    if (email) {
+      user.email=email
+    }
   }
 }
 
@@ -36,7 +67,7 @@ router.get('/', function (req, res) {
 
     data: {
       users: {
-        list: Ueser.getList(),
+        list: User.getList(),
         isEmpty: list.length === 0,
       }
     }
@@ -55,12 +86,41 @@ router.post('/user-create', function (req, res) {
   const user = new User( email, login, password );
 
   User.add(user)
-
   console.log(User.getList())
-  res.render('user-create', {
-    style: 'user-create',
+  res.render('success-info', {
+    style: 'success-info',
+    info:'Користувач створений'
   })
 })
+// ================================================================
+
+router.get('/user-delete', function (req, res) {
+  const { id } = req.query
+  User.deleteById(Number(id))
+  res.render('success-info', {
+    style: 'success-info',
+    info:'Користувач видалений',
+  }) 
+}) 
+
+// ================================================================
+
+router.post('/user-update', function (req, res) {
+  const { email, password, id } = req.body
+
+  let result = false
+
+  const user = User.getById(Number(id))
+
+  if (user.verifyPassword(password)) {
+    User.update(user, { email })
+    result = true
+  }
+  res.render('success-info', {
+    style: 'success-info',
+    info: result ? 'Пошта оновлена': 'Сталася поммилка',
+  }) 
+}) 
 
 
 // Підключаємо роутер до бек-енду
