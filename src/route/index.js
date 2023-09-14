5ad10023a1d9a08160a451107af1b281d3e457d6
p1 <you@example.com>
2023-09-14 11:22:33 +0300

    problem

--- src/route/index.js
+++ src/route/index.js
@@ -5,21 +5,63 @@ const router = express.Router()
 
 // ================================================================
 
+class User {
+  static #list = []
+  constructor(email, login, password) {
+    this.email = email
+    this.login = login
+    this.password = password
+  }
+  static add = (user) => {
+    this.#list.push(user)
+  }
+  static getList = () => {
+    return this.#list
+  }
+}
+
+// ================================================================
+
+
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
-
+  const list = User.getList()
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('index', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+
     style: 'index',
+
+    data: {
+      users: {
+        list: Ueser.getList(),
+        isEmpty: list.length === 0,
+      }
+    }
+
   })
   // ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
+
+
+router.post('/user-create', function (req, res) {
+
+  const { email, login, password } = req.body
+
+  const user = new User( email, login, password );
+
+  User.add(user)
+
+  console.log(User.getList())
+  res.render('user-create', {
+    style: 'user-create',
+  })
+})
+
 
 // Підключаємо роутер до бек-енду
 module.exports = router
